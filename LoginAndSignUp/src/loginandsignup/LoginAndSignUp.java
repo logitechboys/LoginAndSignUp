af34416124619e2c68f2f83d9e8d036eb941f21d
logitechboys <158122061+logitechboys@users.noreply.github.com>
2024-01-30 11:09:04 +0800

    Add files via upload

---
+++ LoginAndSignUp/src/loginandsignup/LoginAndSignUp.java
@@ -0,0 +1,17 @@
+
+package loginandsignup;
+
+
+public class LoginAndSignUp {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LOGIN LoginFrame = new LOGIN();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+        
+    }
+    
+}
